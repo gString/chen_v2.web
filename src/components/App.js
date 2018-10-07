614ab12fe1408622d83c0c1978c1e9a24e68b44a
gush <itcstudio@gmail.com>
2018-10-07 21:23:26 +0300

    start routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import ServicesLayout from "./service-page/Services.page";
+import HomeLayout from "./home-page/Home.page";
 
 class App extends Component {
 	constructor (props) {
@@ -12,11 +14,13 @@ class App extends Component {
 	}
 	render () {
 		return (
-			
+			<BrowserRouter>
 				<div className="App">
 					CHEN V2 - local state
-					<ServicesLayout/>
+					<Route exact path='/' component={HomeLayout} />
+					<Route path='/services' component={ServicesLayout}/>
 				</div>
+			</BrowserRouter>
 				
 		);
 	}
---
+++ src/components/home-page/Home.page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HomeLayout = () => <h3>Home page</h3>;
+
+export default HomeLayout;
